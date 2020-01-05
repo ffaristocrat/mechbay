2f0522371ccd5a84396a4ba58ac47b4646627739
Micheal Keane <cbe83325869822593ea1684a8b4ad63de2479ab6>
2020-01-05 17:48:05 -0500

    write pkd

--- mechbay/pkd.py
+++ mechbay/pkd.py
@@ -7,7 +7,46 @@ class PKDArchive(GundamDataFile):
     header = b"\x20\x44\x4B\x50\x00\x01\x03\x00"
 
     def write(self, records: List[Dict]) -> bytes:
-        pass
+        padding = 64
+        string_bytes = bytes()
+
+        string_bytes += self.header
+        record_count = len(records)
+        string_bytes += int(record_count).to_bytes(4, byteorder="little")
+        # Maybe number of bits for long? Always seems to 0x40000000
+        string_bytes += (64).to_bytes(4, byteorder="little")
+
+        # index size
+        index_size = (record_count * 12) + sum([len(r["filename"].encode("utf-8")) + 1 for r in records])
+        string_bytes += index_size.to_bytes(4, byteorder="little")
+
+        # initial pointers
+        name_start = record_count * 12
+        file_start = index_size + 20
+        file_start += padding - (file_start % padding)
+        for record in records:
+            # File pointer
+            string_bytes += file_start.to_bytes(4, byteorder="little")
+
+            # File size
+            string_bytes += len(record["bytes"]).to_bytes(4, byteorder="little")
+            file_start += len(record["bytes"])
+            # Pad to 64 byte increments
+            file_start += padding - (file_start % padding)
+
+            # name pointer
+            string_bytes += name_start.to_bytes(4, byteorder="little")
+            name_start += len(record["filename"].encode("utf-8")) + 1
+
+        for record in records:
+            string_bytes += record["filename"].encode("utf-8") + b"\x00"
+        string_bytes += (padding - (len(string_bytes) % padding)) * b"\x00"
+
+        for record in records:
+            string_bytes += record["bytes"]
+            string_bytes += (padding - (len(string_bytes) % padding)) * b"\x00"
+
+        return string_bytes
 
     def read(self, buffer: BinaryIO) -> List[Dict]:
         record_count = self.read_header(buffer)
