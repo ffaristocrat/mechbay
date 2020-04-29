a25538c600ece64e6abaa5109e297f45c1246b52
Micheál Keane <ffaristocrat@gmail.com>
2020-04-29 17:04:04 -0400

    formatting tweaks

--- unpack_string_tbl.py
+++ unpack_string_tbl.py
@@ -7,16 +7,17 @@ from mechbay import StringTBL
 
 def main():
     parser = argparse.ArgumentParser(description="Unpack all string TBL files")
-    parser.add_argument(
-        "tbl", type=str, help="Files to unpack"
-    )
+    parser.add_argument("tbl", type=str, help="Files to unpack")
     parser.add_argument(
         "--path", type=str, help="Root directory to search from", default="."
     )
     args = parser.parse_args()
+
     search_path = os.path.join(args.path, "**", args.tbl)
     print(search_path)
+
     paths = glob.glob(search_path, recursive=True)
+
     for string_filename in paths:
         print(string_filename)
         try:
